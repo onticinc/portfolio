0fd03feee3cb5880f231f3cc9247790ebc9cc3fb
onticinc <onticinc@gmail.com>
2023-03-10 11:14:53 -0700

    removed old folder

--- new/src/pages/index.jsx
+++ new/src/pages/index.jsx
@@ -11,41 +11,41 @@ import Bio from '@/components/Bio'
 import Avatar from '@/images/avatar.png'
 import Image from 'next/image'
 
-
 export default function Home() {
-
-
   return (
     <>
       <Head>
         <title>Michael Smith - Create, Document, Produce. </title>
-        <meta name="portfolio" content="Portfolio of work by Michael Smith." /> 
+        <meta name="portfolio" content="Portfolio of work by Michael Smith." />
       </Head>
 
-
-      <div className="flex flex-row justify-center lg:mt-20 items-center">
-        <div id="avatar" className="col-span-1 ml-20">
-          <Image src={Avatar} className="rounded-md bg-zinc-100 dark:bg-zinc-800 w-full "/> 
-          <h4>Bering Sea - Nome AK</h4>
+      <div className="p-10">
+        <div className="flex flex-row items-center justify-center lg:mt-20">
+          <div id="avatar" className="col-span-1 justify-center lg:w-1/3">
+            <Image
+              src={Avatar}
+              className="w-full rounded-md bg-zinc-100 dark:bg-zinc-800 "
+            />
+            <h4 className="dark:text-zinc-400">Bering Sea - Nome AK</h4>
+          </div>
+          <div className="w-full lg:w-2/3">
+            <div id="bio" className="col-span-1 m-36">
+              <Bio />
+              <SocialLinks />
+            </div>
+          </div>
         </div>
-       <div className="w-full lg:w-2/3">
-          <div id="bio" className="col-span-1 m-36">
-            <Bio />
-            <SocialLinks />
-          </div>    
-        </div>
-      </div>  
+      </div>
       <div className="flex flex-col">
-          <ImageSlider />      
+        <ImageSlider />
       </div>
 
-      <div className="flex flex-row p-8 mb-36">
-       
-       {/* PUT ARTICLE HERE */}
-        <Container class="w-1/2 h-full">
+      <div className="mb-36 flex flex-row p-8">
+        {/* PUT ARTICLE HERE */}
+        <Container class="h-full w-1/2">
           <Resume />
         </Container>
-        <Container class="w-1/2 h-full">
+        <Container class="h-full w-1/2">
           <Newsletter />
         </Container>
       </div>
