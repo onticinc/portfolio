ff2ac172e384ccaaf5bea3e79550edb0e94d80d6
onticinc <onticinc@gmail.com>
2023-03-30 14:13:13 -0600

    debugged typescript problems

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
-import nextMDX from '@next/mdx'
-import remarkGfm from 'remark-gfm'
-import rehypePrism from '@mapbox/rehype-prism'
+
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -19,13 +17,4 @@ const nextConfig = {
   },
 };
 
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypePrism],
-  },
-})
-
-export default withMDX(nextConfig)
 
