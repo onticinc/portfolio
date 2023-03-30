5a10c10b35bfb96f3ff88675a2840d761a4ee435
onticinc <onticinc@gmail.com>
2023-03-30 01:27:51 -0600

    updated tsconfig and fixed image loader

--- src/components/CustomLoader.tsx
+++ src/components/CustomLoader.tsx
@@ -1,3 +1,24 @@
 import type { ImageLoader } from "next/image";
 import Image from "next/image";
+import React from "react";
 import { unsplashLoader } from "../utils";
+
+export default function CustomLoader(props: {
+    src: string;
+    width: number;
+    height: number;
+    alt: string;
+  }) {
+    return (
+      <div>
+        <Image
+          loader={unsplashLoader}
+          src={props.src}
+          width={props.width}
+          height={props.height}
+          alt={props.alt}
+        />
+      </div>
+    );
+  }
+  
