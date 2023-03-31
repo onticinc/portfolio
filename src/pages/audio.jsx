b295613456a64f22fba9b7d1f33130ed35a09dc2
onticinc <onticinc@gmail.com>
2023-03-31 16:21:40 -0600

    setting up youtube api on audio page.

--- src/pages/audio.jsx
+++ src/pages/audio.jsx
@@ -1,13 +1,24 @@
 import Head from 'next/head'
 
+const YOUTUBE_PLAYLIST_ITEMS_API = "https://www.googleapis.com/youtube/v3/playlistItems";
+
+export async function getStaticProps() {
+  const res = await fetch(YOUTUBE_PLAYLIST_ITEMS_API); 
+  const data = await res.json();
+  return {
+    props: {
+      data
+    }
+  }
+}
 
 
-
-export default function Speaking() {
+export default function Audio({ data }) {
+  console.log('data', data)
   return (
     <>
       <Head>
-        <title>Speaking - Spencer Sharp</title>
+        <title>Audio Projects - Michael Smith</title>
         <meta
           name="description"
           content="I have been able to record many great bands over the years, here are a few of my favorites. "
