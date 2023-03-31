8c9da0bb8c213f9653a3289b2135353ceb75ead8
onticinc <onticinc@gmail.com>
2023-03-31 13:45:45 -0600

    got featured project component working

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -3,18 +3,18 @@ export default function BioCard(){
   return(
         <>
           <div className="text-base text-zinc-600 dark:text-zinc-400 w-full container md:pt-5 lg:pt-0 lg:pl-5">
-              <h1 id="title" className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-                Create, Document, Produce.
-              </h1>
-              <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-                Software developer, Audio Engineer, and Podcast connoisseur. 
-              </p>
-              <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-                Focused on MERN stack with Next JS 13,
-                TypeScript, and a little bit of python when needed. 
-              </p>
-                
-            </div>
-        </>
+            <h1 id="title" className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+              Create, Document, Produce.
+            </h1>
+            <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+              Software developer, Audio Engineer, and Podcast connoisseur. 
+            </p>
+            <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+              Focused on MERN stack with Next JS 13,
+              TypeScript, and a little bit of python when needed. 
+            </p>
+              
+          </div>
+      </>
     )
 }
--- src/components/FeaturedProject.jsx
+++ src/components/FeaturedProject.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import podcast_template_01 from "@/images/software/podcast_template_01.png";
 
 export default function FeaturedProject() {
@@ -6,12 +8,13 @@ export default function FeaturedProject() {
       <div id="avatar" className="m-4 md:col-span-2">
         <div className="w-full justify-center rounded-2xl border border-zinc-100 p-10   dark:border-zinc-700/40 ">
           <h1 className="mt-3 hidden text-xl dark:text-zinc-400 lg:block">
-            Featured Project:{" "}
+            Featured Project: 
           </h1>
           {/* <h4 className="dark:text-zinc-400 hidden mt-3 italic lg:block">Randomly Generated </h4> */}
-          <div className="lg:flex lg:flex-auto lg:flex-row">
+          <div className="lg:flex lg:flex-auto lg:flex-row mt-5">
             <Image src={podcast_template_01} alt="Podcast Template" />
           </div>
+
         </div>
       </div>
     </>
