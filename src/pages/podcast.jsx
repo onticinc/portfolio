16b96503a1646ba2649df859f7c23bd404bcd49f
onticinc <onticinc@gmail.com>
2023-04-06 18:00:24 -0600

    setting up tabs on podcast page

--- src/pages/podcast.jsx
+++ src/pages/podcast.jsx
@@ -46,6 +46,7 @@ export default function Podcast({ data }) {
     const currentIndex = data.items.findIndex(
       (item) => item.snippet.resourceId.videoId === currentVideo.snippet.resourceId.videoId
     );
+
     setCurrentVideo(data.items[currentIndex + 1]);
     setPlaying(false);
   };
@@ -58,12 +59,47 @@ export default function Podcast({ data }) {
     setPlaying(false);
   };
 
+  function hidePreviousButton() {
+    if (currentVideo === data.items[0]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   function handlePageChange(page) {
     const lastPostIndex = currentPage * postsPerPage;
     const firstPostIndex = lastPostIndex - postsPerPage;
     const currentPosts = data.items.slice(firstPostIndex, lastPostIndex);
     setCurrentPage(page);
   }
+
+  /*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+const tabs = [
+  { name: 'Audio Only', href: '#', current: false },
+  { name: 'Audio and Video', href: '#', current: false }
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+  
+
+
 
   
   return (
@@ -79,6 +115,47 @@ export default function Podcast({ data }) {
         title="Eggs The Podcast"
         intro="Co-Host and Business Partner."
       >
+
+
+      <div className="sm:hidden">
+        <label htmlFor="tabs" className="sr-only">
+          Select a tab
+        </label>
+        {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
+        <select
+          id="tabs"
+          name="tabs"
+          className="block w-full rounded-md mb-10 py-2 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+          defaultValue={tabs.find((tab) => tab.current)}
+        >
+          {tabs.map((tab) => (
+            <option key={tab.name}>{tab.name}</option>
+          ))}
+        </select>
+      </div>
+      <div className="hidden sm:block">
+        <div className="border-b border-gray-200 mb-5">
+          <nav className="-mb-px flex space-x-8" aria-label="Tabs">
+            {tabs.map((tab) => (
+              <a
+                key={tab.name}
+                href={tab.href}
+                className={classNames(
+                  tab.current
+                    ? 'border-indigo-500 text-indigo-600'
+                    : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
+                  'whitespace-nowrap border-b-2 py-4 text-sm font-medium'
+                )}
+                aria-current={tab.current ? 'page' : undefined}
+              >
+                {tab.name}
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+
+
 
         {/* Video Player */}
         <div className="justify-center border border-zinc-100 p-5 dark:border-zinc-700/40 lg:-ml-9 lg:mb-10 lg:-mr-9 mb-10">
