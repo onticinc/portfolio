ca7bd225a01aaec6bc0ef6b85d4a00681ed9620f
onticinc <onticinc@gmail.com>
2024-07-14 15:55:22 -0600

    updated feature project'

--- src/components/FeaturedProject.jsx
+++ src/components/FeaturedProject.jsx
@@ -5,13 +5,14 @@ import dynamic from 'next/dynamic';
 const YoutubeVideoPlayer = dynamic(() => import('@/components/youtubePlayer'), { ssr: false });
 
 export default function FeaturedProject() {
-    const videoId = '5mT5s8EQkj4&t=350s'; // The ID of the video you want to play
+    const videoId = 'nbpQbcp1jHo'; // The ID of the video you want to play
+    
 
     return (
         <div className="featured-project">
             <div className="bg-zinc-200 p-1 shadow dark:border-zinc-700/40 dark:bg-zinc-700">
                 <div className="rounded-lg border-zinc-100 bg-neutral-50 p-3 dark:bg-zinc-800">
-                    <h2 className="text-sm font-semibold text-zinc-900 dark:text-zinc-100 mb-2">Featured Project</h2>
+                    <h2 className="text-sm font-semibold text-zinc-900 dark:text-zinc-100 mb-2"><span className="font-bold">Featured Project:</span> Lighting setup time lapse, live Recording of Layne Bowen at the event.</h2> 
                     <YoutubeVideoPlayer id={videoId} playing={false} />
                 </div>
             </div>
