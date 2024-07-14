5783fb5a63dd515a239c2a780ce2a71fb55cd2c4
onticinc <onticinc@gmail.com>
2024-07-14 17:42:08 -0600

    fixed layout on home page

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -2,23 +2,25 @@ export default function BioCard(){
   
   return(
         <>
-          <div className="text-base text-zinc-600 dark:text-zinc-400 w-full container mt-10">
+          <div className="text-base text-zinc-600 dark:text-zinc-400 w-full container">
+            <div className="p-3 mt-3 bg-zinc-200 dark:bg-zinc-900 rounded-lg">
             <h1 id="title" className="text-3xl font-bold tracking-tight text-zinc-800 dark:text-zinc-300 sm:text-5xl">
               Create, Document, Produce.
             </h1>
             <p className="mt-3 text-xl font-bold text-zinc-600 dark:text-zinc-400">
               Software developer, Audio Engineer, and Event Professional. 
             </p>
-            <p className="mt-10 text-base text-zinc-600 dark:text-zinc-400">
+            <p className=" text-base mt-5 text-zinc-600 dark:text-zinc-400">
               <span className="font-bold">Software:</span> Focused on MERN | JAM stack with Next JS,
               TypeScript, and Python when needed. 
             </p>
-            <p className="mt-10 text-base text-zinc-600 dark:text-zinc-400">
-              <span className="font-bold">Audio & Video:</span> Software: Protools, Reaper, Ableton, Creative Cloud. Hardware: Midas m32, Behringer x32, Yamaha LS9. 
+            <p className="mt-3 text-base text-zinc-600 dark:text-zinc-400">
+              <span className="font-bold">Editing:</span> Protools, Reaper, Ableton, Creative Cloud.  
             </p>
-            <p className="mt-9 text-base text-zinc-600 dark:text-zinc-400">
-              <span className="font-bold">Lighting:</span> ShowExpress, Hog-500.  
+            <p className="mt-3 text-base text-zinc-600 dark:text-zinc-400">
+              <span className="font-bold">Hardware:</span> ShowExpress, Hog-500, Midas M32, Behringer x32, Chauvet Products, QSC K Series.  
             </p>
+            </div>
               
           </div>
       </>
--- src/components/ShortIntro.jsx
+++ src/components/ShortIntro.jsx
@@ -5,18 +5,20 @@ import Bio from "@/components/Bio";
 export default function ShortIntro() {
   return (
     <>
-      <div className="mb-2 justify-center rounded-sm border border-zinc-200 bg-zinc-200 shadow first:w-full  dark:border-zinc-700/40 dark:bg-zinc-700">
+      <div className="justify-center rounded-sm border border-zinc-200 bg-zinc-200 shadow first:w-full  dark:border-zinc-700/40 dark:bg-zinc-700">
         <div className="m-1 rounded-lg bg-zinc-100 dark:bg-zinc-800">
-          <div className="col rounded-lg p-5 dark:bg-zinc-800 sm:col-span-1 md:col-span-4 ">
+          <div className="col rounded-lg p-3 dark:bg-zinc-800 sm:col-span-1 md:col-span-4 ">
             <div className="col-span-3">
+            <div className="p-3 bg-zinc-200 dark:bg-zinc-900 rounded-lg">
               <Image
                 src={Avatar}
-                alt="Mike Smith - Nome AK"
-                className=" rounded-lg object-cover max-h-[500px] w-full"
+                alt="Photo of Michael Smith - Nome AK"
+                className="rounded-lg object-cover max-h-[500px] w-full"
               />
-              <h4 className="dark:text-zinc-600 mt-1 text-right">
+              <h4 className="dark:text-zinc-500 mt-3 text-right">
                 Michael Smith - Bering Sea, Nome AK
               </h4>
+            </div>
             </div>
             <div className="col-span-3 ">
               <Bio />
